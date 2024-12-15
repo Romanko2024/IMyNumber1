506e0f6d8c113bd9b57b1d2df57b47417fbe0cd2
Roman <manko.roman1123@vu.cdu.edu.ua>
2024-12-15 17:59:40 +0200

    Клас MyFrac

--- IMyNumber/IMyNumber/Program.cs
+++ IMyNumber/IMyNumber/Program.cs
@@ -1,7 +1,27 @@
-﻿interface IMyNumber<T> where T : IMyNumber<T>
+﻿using System.Numerics;
+
+interface IMyNumber<T> where T : IMyNumber<T>
 {
     T Add(T b);
     T Subtract(T b);
     T Multiply(T b);
     T Divide(T b);
+}
+public class MyFrac : IMyNumber<MyFrac>, IComparable<MyFrac>
+{
+    //numerator/denominator
+    private BigInteger nom; 
+    private BigInteger denom;
+
+    // констркуктор що ініціалізує дріб за nom denom
+    public MyFrac(BigInteger nom, BigInteger denom)
+    {
+        //
+        if (denom == 0) throw new DivideByZeroException("Знаменник не може бути нулем.");
+        //присвоєння значень 
+        this.nom = nom; 
+        this.denom = denom; 
+
+        Simplify(); // треба зробюити цей метод
+    }
 }
