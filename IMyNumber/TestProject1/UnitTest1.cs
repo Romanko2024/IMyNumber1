91f037360e5a04a55f049a23218e2e399875fc42
Roman <manko.roman1123@vu.cdu.edu.ua>
2024-12-16 14:04:10 +0200

    test for CorrectSum & CorrectDifference

--- IMyNumber/TestProject1/UnitTest1.cs
+++ IMyNumber/TestProject1/UnitTest1.cs
@@ -8,5 +8,21 @@ namespace TestProject1
             var frac = new MyFrac(2, 4);
             Assert.Equal("1/2", frac.ToString());
         }
+        [Fact]
+        public void CorrectSum()
+        {
+            var frac1 = new MyFrac(1, 3);
+            var frac2 = new MyFrac(1, 6);
+            var result = frac1.Add(frac2);
+            Assert.Equal("1/2", result.ToString());
+        }
+        [Fact]
+        public void CorrectDifference()
+        {
+            var frac1 = new MyFrac(1, 2);
+            var frac2 = new MyFrac(1, 3);
+            var result = frac1.Subtract(frac2);
+            Assert.Equal("1/6", result.ToString());
+        }
     }
 }
